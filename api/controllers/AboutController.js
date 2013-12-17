1c6cff34fda0f111529cc978cfe50692355e7e1b
Shaun O'Connor <shaun@shaun-oconnor.com>
2013-12-17 16:03:11 +1100

    Update controllers, hacking

--- api/controllers/AboutController.js
+++ api/controllers/AboutController.js
@@ -4,23 +4,16 @@
  * @module      :: Controller
  * @description :: A set of functions called `actions`.
  *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
 var AboutController = {
 
   index: function(req, res){
 
+    sails.log('I am AboutController');
+
     return res.view({
-      things:[{name: 'people'}, {name:'location'}, {name:'blog'}]
+      things:[{name: 'people'}, {name:'location'}, {name:'blog'}, {name:'another'},{name: 'Twitter'}]
     });
 
   }
